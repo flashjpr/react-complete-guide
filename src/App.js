31ab4c14c25a0e32756adee0f31050368150d9dd
flashjpr <flashjpr@gmail.com>
2019-06-02 16:58:10 +0100

    State manipulations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Alex', age: 24},
+      {name: 'Larisa', age: 24},
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log('Clicked')
+    this.setState({
+      persons: [
+        {name: 'Alexandru', age: 29},
+        {name: 'Larisa', age: 21},
+      ]
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+        >Favourite food: sushi</Person>
       </div>
     );
-    // ^ compiles to bellow:
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a react App'))
   }
 }
 
